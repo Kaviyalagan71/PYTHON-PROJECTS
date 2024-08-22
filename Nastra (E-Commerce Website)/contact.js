7cc6113ac02ffa82b02fb58d6a7dd965f51f848b
Nithish-Kumar7 <132042833+Nithish-Kumar7@users.noreply.github.com>
2024-08-22 12:15:26 +0530

    My first website

---
+++ Nastra (E-Commerce Website)/contact.js
@@ -0,0 +1,14 @@
+<script>
+
+    var sidenav = document.querySelector(".side-navbar")
+
+    function shownavbar() {
+        sidenav.style.left = "0"
+    }
+
+    function closenavbar() {
+        sidenav.style.left = "-60%"
+
+    }
+
+    </script>
